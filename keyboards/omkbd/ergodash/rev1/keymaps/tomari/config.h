1375d343bc4c9898d005553fe9983380d5931145
Hisanobu Tomari <posco.grubb@gmail.com>
2025-04-29 06:40:34 +0900

    ergodash: increase tapping term to 500ms

--- keyboards/omkbd/ergodash/rev1/keymaps/tomari/config.h
+++ keyboards/omkbd/ergodash/rev1/keymaps/tomari/config.h
@@ -35,5 +35,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
 #define LAYER_STATE_8BIT
-#define RETRO_TAPPING
-
+#define TAPPING_TERM 500
