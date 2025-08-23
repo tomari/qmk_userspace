c549ce2c94da86a2f131b98dc169f40e4e4a5b05
Hisanobu Tomari <posco.grubb@gmail.com>
2025-08-24 08:47:21 +0900

    Ergodash: modify alt layout

--- keyboards/omkbd/ergodash/rev1/keymaps/tomari/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/tomari/keymap.c
@@ -27,11 +27,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT(
-           KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                        KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-           KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-           KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LALT,                        KC_RALT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-           KC_ESC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LGUI,                        KC_APP,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,
-        TT(_UPPER), KC_F12,  KC_DEL,  KC_RCTL, KC_LNG2, KC_LSFT, KC_SPC,                        KC_ENT,MO(_LOWER), KC_LNG1, TT(_NUM),KC_LEFT, KC_RGHT, KC_DOWN
+           KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_F4,                           KC_F3,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
+           KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_F1,                           KC_F2,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+           KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS,                        KC_EQL,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+           KC_ESC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LGUI,                         KC_F5,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,
+        TT(_UPPER), KC_F12,  KC_DEL,  KC_LALT, KC_LNG2, KC_LSFT, KC_SPC,                        KC_ENT,MO(_LOWER), KC_LNG1, TT(_NUM),KC_LEFT, KC_RGHT, KC_DOWN
   ),
   /* Lower
    * ,----------------------------------------------------------------------------------------------------------------------.
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______, _______,
     _______, KC_WBAK, KC_UP,   KC_WFWD, KC_PGUP, _______, _______,                        _______, _______, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS,
     _______, KC_LEFT, KC_DOWN, KC_RIGHT,KC_PGDN, _______, _______,                        _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______,                        KC_APP,  _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______,          KC_LALT, _______, KC_BSPC,      _______, _______, KC_RALT,          _______, _______, _______, _______
   ),
 
